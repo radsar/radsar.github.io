6b4f294982ffcbab686aff319b7be36e6f415cb0
Kavitha Saravanan <kavitha_saravanan@comcast.com>
2024-07-08 12:01:54 +0530

    deploy docs

--- scripts/publishSitemap.js
+++ scripts/publishSitemap.js
@@ -7,7 +7,7 @@ const publicObj = getPathsObject('public/');
 const today = formatDate(new Date());
 const urlNode = (path) => `
   <url>
-    <loc>https://comcast.github.io${path}</loc>
+    <loc>https://radsar.github.io${path}</loc>
     <lastmod>${
   pagesObj[path].lastModified
     ? formatDate(new Date(pagesObj[path].lastModified))
@@ -19,14 +19,14 @@ const urlNode = (path) => `
 const sitemapXml = `<?xml version='1.0' encoding='UTF-8'?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
-    <loc>https://comcast.github.io/</loc>
+    <loc>https://radsar.github.io/</loc>
     <lastmod>${pagesObj['/index'].lastModified
     ? formatDate(new Date(pagesObj['/index'].lastModified))
     : today}</lastmod>
     <changefreq>daily</changefreq>
   </url>${Object.keys(pagesObj).map((path) => (path.includes('index') || path.includes('_') || path.includes('404') || path.includes('sitemap.xml') || path.includes('styles') ? '' : urlNode(path))).join('')}
   <url>
-    <loc>https://comcast.github.io/humans.txt</loc>
+    <loc>https://radsar.github.io/humans.txt</loc>
     <lastmod>${publicObj['/humans'].lastModified
     ? formatDate(new Date(publicObj['/humans'].lastModified))
     : today}</lastmod>
